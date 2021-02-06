34175a6e475943fb36f2f21092ecc1d35a07e3b4
Evan <ehutch7@gmail.com>
2021-02-06 08:16:50 -0500

    Building ActivityListItem component.

--- client-app/src/features/activities/dashboard/ActivityList.jsx
+++ client-app/src/features/activities/dashboard/ActivityList.jsx
@@ -1,55 +1,17 @@
 import { observer } from "mobx-react-lite";
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
-import { Item, Button, Label, Segment } from "semantic-ui-react";
+import { Item, Segment } from "semantic-ui-react";
 import { ActivityContext } from "../../../app/stores/activityStore";
+import { ActivityListeItem } from "./ActivityListItem";
 
 export const ActivityList = observer(() => {
     const activityStore = useContext(ActivityContext);
-    const {
-        activitiesByDate,
-        deleteActivity,
-        submitting,
-        target,
-    } = activityStore;
+    const { activitiesByDate } = activityStore;
     return (
         <Segment clearing>
             <Item.Group divided>
                 {activitiesByDate.map((activity) => (
-                    <Item key={activity.id}>
-                        <Item.Content>
-                            <Item.Header as="a">{activity.title}</Item.Header>
-                            <Item.Meta>{activity.date}</Item.Meta>
-                            <Item.Description>
-                                <div>{activity.description}</div>
-                                <div>
-                                    {activity.city}, {activity.venue}
-                                </div>
-                            </Item.Description>
-                            <Item.Extra>
-                                <Button
-                                    as={Link}
-                                    to={`/activities/${activity.id}`}
-                                    floated="right"
-                                    content="View"
-                                    color="blue"
-                                />
-                                <Button
-                                    name={activity.id}
-                                    onClick={(e) =>
-                                        deleteActivity(e, activity.id)
-                                    }
-                                    loading={
-                                        submitting && target === activity.id
-                                    }
-                                    floated="right"
-                                    content="Delete"
-                                    color="red"
-                                />
-                                <Label basic content={activity.category} />
-                            </Item.Extra>
-                        </Item.Content>
-                    </Item>
+                    <ActivityListeItem activity={activity} key={activity.id} />
                 ))}
             </Item.Group>
         </Segment>
