5ca2e5b62998ebc7ea8e030f7c2e666e16366467
Evan <ehutch7@gmail.com>
2021-02-11 15:11:17 -0500

    Creating RestException class derived from Exception.

---
+++ Application/Errors/RestException.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Net;
+
+namespace Application.Errors
+{
+    public class RestException : Exception
+    {
+        public HttpStatusCode StatusCode { get; }
+        public object Errors { get; }
+        public RestException(HttpStatusCode statusCode, object errors = null)
+        {
+            this.StatusCode = statusCode;
+            this.Errors = errors;
+        }
+    }
+}
